84e4c890bc93319cacf693a7551f73eb03eb4981
CallumBoase <callum.boase@gmail.com>
2025-04-11 11:17:07 +1000

    tidy-up

--- src/helpers/index.ts
+++
@@ -1,7 +0,0 @@
-export function hello() {
-  console.log("Hello from the helpers object!");
-}
-
-export function add(a: number, b: number) {
-  return a + b;
-}
--- src/main.ts
+++ src/main.ts
@@ -11,11 +11,11 @@ $(document).ready(function () {
   console.log("The KnackVibeCodingLibraryscript is running!");
 });
 
-// CUSTOM CODE STARTS HERE
+// APPLY CUSTOM UI TO THE KNACK APP
 import { home } from './ui/home/index';
 
 // APPLY THE CUSTOM HOME PAGE TO VIEW_1 SCENE_1 (HOME PAGE)
 // Replacing the div with id "customHomePage" with the custom home page
 $(document).on('knack-view-render.view_1', function(_event: any, _view: any, _records: any){
   $('#customHomePage').html(home());
-})
+});
